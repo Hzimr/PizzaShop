6a9612a5984d56b01b4c213ade1af21262f8a3b6
Hítalo Rodrigues <hitalo005@gmail.com>
2024-05-28 15:39:44 -0300

    refactor: changed name file and path

--- src/pages/auth/signIn.tsx
+++ src/pages/auth/signIn.tsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
-import { signIn } from '@/api/sign-in'
+import { signIn } from '@/api/signIn'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
