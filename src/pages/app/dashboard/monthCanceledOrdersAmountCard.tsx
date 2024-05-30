fe7be68d112a5146eefe4f64693fbd94df8affdc
Hítalo Rodrigues <hitalo005@gmail.com>
2024-05-30 16:33:04 -0300

    feat: created useQuery in files

--- src/pages/app/dashboard/monthCanceledOrdersAmountCard.tsx
+++ src/pages/app/dashboard/monthCanceledOrdersAmountCard.tsx
@@ -1,8 +1,15 @@
+import { useQuery } from '@tanstack/react-query'
 import { DollarSign } from 'lucide-react'
 
+import { getMonthCanceledOrdersAmount } from '@/api/getMonthCanceledOrdersAmounth'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
 export function MonthCanceledOrdersAmountCard() {
+  const { data: monthCanceledOrdersAmount } = useQuery({
+    queryFn: getMonthCanceledOrdersAmount,
+    queryKey: ['metrics', 'month-canceled-order-amount'],
+  })
+
   return (
     <Card>
       <CardHeader className="flex-row items-center justify-between space-y-0 pb-2">
@@ -12,11 +19,31 @@ export function MonthCanceledOrdersAmountCard() {
         <DollarSign className="h-4 w-4 text-muted-foreground" />
       </CardHeader>
       <CardContent className="space-y-1">
-        <span className="text-2xl font-bold tracking-tight">48</span>
-        <p className="text-xs text-muted-foreground">
-          <span className="text-emerald-500 dark:text-emerald-400">-2%</span> em
-          relação ao mês passado
-        </p>
+        {monthCanceledOrdersAmount && (
+          <>
+            <span className="text-2xl font-bold tracking-tight">
+              {monthCanceledOrdersAmount.amount.toLocaleString('pt-BR')}
+            </span>
+            <p className="text-xs text-muted-foreground">
+              {monthCanceledOrdersAmount.diffFromLastMonth < 0 && (
+                <div className="flex gap-2">
+                  <span className="text-emerald-500 dark:text-emerald-400">
+                    +{monthCanceledOrdersAmount.diffFromLastMonth}%
+                  </span>
+                  <p>relação ao mês passado</p>
+                </div>
+              )}
+              {monthCanceledOrdersAmount.diffFromLastMonth >= 0 && (
+                <div className="flex gap-2">
+                  <span className="text-rose-500 dark:text-rose-400">
+                    -{monthCanceledOrdersAmount.diffFromLastMonth}%
+                  </span>
+                  <p>relação ao mês passado</p>
+                </div>
+              )}
+            </p>
+          </>
+        )}
       </CardContent>
     </Card>
   )
